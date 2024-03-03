426729673b20381c7d3de6661b19407b94b31009
mrtqztk37 <mrtqztk37@gmail.com>
2024-03-03 11:41:57 +0300

    proje sonu

---
+++ README.md
@@ -0,0 +1,10 @@
+# Müzik Player Projesi
+
+Bu proje, HTML, CSS ve JavaScript kullanılarak geliştirilmiş etkileyici bir müzik çaları içerir.
+
+# Gif
+
+<img src="images/screen.gif" />
+
+# music-player
+# music-player
---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+  {
+    name: "Dilber",
+    path: "musics/Song 1.mp3",
+    artist: "Burak King & Ceren Sagu",
+    cover: "images/cover 1.png",
+  },
+  {
+    name: "İmdadım",
+    path: "musics/Song 2.mp3",
+    artist: "Zara & Eypio",
+    cover: "images/cover 2.png",
+  },
+  {
+    name: "Aklında Birileri Var",
+    path: "musics/Song 3.mp3",
+    artist: "Bedo x Mert Şenel ",
+    cover: "images/cover 3.png",
+  },
+  {
+    name: "Benim İçin Üzülme",
+    path: "musics/Song 4.mp3",
+    artist: "Deeperise & Jabbar",
+    cover: "images/cover 4.png",
+  },
+  {
+    name: "Aramazsan Arama ",
+    path: "musics/Song 5.mp3",
+    artist: "Velet & Decrat",
+    cover: "images/cover 5.png",
+  },
+];
